f730ada703db900dd5a520c01e921577e6f919bc
Julien Matondo <julien.matondo-tezolo@bothive.be>
2022-06-13 22:31:28 +0200

    finished product page

--- lib/view/screens/product_screen.dart
+++ lib/view/screens/product_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:selecteat_app/utils/constants.dart';
 import 'package:selecteat_app/view/components/bottomnav.dart';
 import 'package:selecteat_app/view/widgets/products_grid.dart';
 import 'package:selecteat_app/viewmodels/products_list_view_model.dart';
@@ -12,20 +13,136 @@ class ProductScreen extends StatefulWidget {
 }
 
 class _ProductScreenState extends State<ProductScreen> {
-
   @override
   void initState() {
     super.initState();
     Provider.of<ProductsListViewModel>(context, listen: false).allProducts();
   }
 
+  final categories = [
+    {
+      'name': 'Fruits et legumes frais',
+      'image': 'assets/icons/fruits.png',
+    },
+    {
+      'name': 'Viande et poissons',
+      'image': 'assets/icons/meatandfish.png',
+    },
+    {
+      'name': 'Cremerie',
+      'image': 'assets/icons/cremerie.png',
+    },
+    {
+      'name': 'Boulangerie',
+      'image': 'assets/icons/bread.png',
+    },
+    {
+      'name': 'Epicerie-sucree',
+      'image': 'assets/icons/sweets.png',
+    },
+    {
+      'name': 'Surgeles',
+      'image': 'assets/icons/frozen.png',
+    },
+    {
+      'name': 'Boissons-et-alcools',
+      'image': 'assets/icons/drinks.png',
+    },
+    {
+      'name': 'Conserves',
+      'image': 'assets/icons/cremerie.png',
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
     var listViewModel = Provider.of<ProductsListViewModel>(context);
+    print(categories[0]["name"]);
 
     return Scaffold(
       bottomNavigationBar: const BottomNav(),
-      body: ProductsGrid(productsList: listViewModel.productsList),
+      body: Stack(
+        children: [
+          SafeArea(
+              child: Column(
+            children: [
+              Container(
+                padding: const EdgeInsets.all(20),
+                color: Colors.white,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Products",
+                        style: Theme.of(context)
+                            .textTheme
+                            .headline6!
+                            .copyWith(fontWeight: FontWeight.bold)),
+                    const SizedBox(height: 20),
+                    SizedBox(
+                      height: 100,
+                      child: ListView.builder(
+                          shrinkWrap: true,
+                          scrollDirection: Axis.horizontal,
+                          itemCount: categories.length,
+                          itemBuilder: (context, index) {
+                            var category = categories[index];
+                            var name = category['name'];
+                            var image = category['image'];
+
+                            return Container(
+                              width: 80,
+                              padding: const EdgeInsets.only(right: 20),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  Container(
+                                    decoration: const BoxDecoration(
+                                      color: brandLightGreyColor,
+                                      borderRadius: BorderRadius.all(
+                                          Radius.circular(10.0)),
+                                    ),
+                                    padding: const EdgeInsets.all(15),
+                                    child: Image(
+                                      image:
+                                          AssetImage(image!),
+                                      height: 30,
+                                    ),
+                                  ),
+                                  const SizedBox(height: 10),
+                                  Text(
+                                    name!,
+                                    softWrap: true,
+                                    maxLines: 2,
+                                    textAlign: TextAlign.center,
+                                    overflow: TextOverflow.ellipsis,
+                                    style: const TextStyle(
+                                      fontSize: 12
+                                    ),
+                                  )
+                                ],
+                              ),
+                            );
+                          }),
+                    )
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        ProductsGrid(productsList: listViewModel.productsList),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          )),
+        ],
+      ),
     );
   }
 }
