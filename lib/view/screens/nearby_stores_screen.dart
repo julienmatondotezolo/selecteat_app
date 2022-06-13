36e20b0c3b53902b79612b955c2c8ac824a41679
Julien Matondo <julien.matondo-tezolo@bothive.be>
2022-06-13 03:55:04 +0200

    adding loader on cart

--- lib/view/screens/nearby_stores_screen.dart
+++ lib/view/screens/nearby_stores_screen.dart
@@ -69,53 +69,72 @@ class _NearbyStoreScreenState extends State<NearbyStoreScreen> {
 
     final _markers = _buildMarkers();
 
+    if (_position == null) {
+      return Scaffold(
+        body: Container(
+          width: size.width,
+          color: brandLightGreyColor,
+          padding: const EdgeInsets.all(40),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+  crossAxisAlignment: CrossAxisAlignment.center,
+            children: const [
+              CircularProgressIndicator(color: brandPrimaryColor),
+              SizedBox(
+                height: 40,
+              ),
+              Text(
+                "Searching all nearby stores..",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 18,
+                  color: brandDarkColor,
+                  fontWeight: FontWeight.bold,
+                ),
+              )
+            ],
+          ),
+        ),
+      );
+    }
+
     return Scaffold(
       bottomNavigationBar: const BottomNav(),
       body: Stack(
         children: [
-          _position != null
-              ? FlutterMap(
-                  options: MapOptions(
-                    onMapCreated: (c) {
-                      _mapController = c;
-                    },
-                    center: LatLng(_position.latitude!, _position.longitude!),
-                    zoom: 14.5,
+          FlutterMap(
+            options: MapOptions(
+              onMapCreated: (c) {
+                _mapController = c;
+              },
+              center: LatLng(_position.latitude!, _position.longitude!),
+              zoom: 14.5,
+            ),
+            mapController: _mapController,
+            nonRotatedLayers: [
+              TileLayerOptions(
+                  urlTemplate:
+                      "https://api.mapbox.com/styles/v1/julienmt/ckx6t9wl86f0u14nqm714f3th/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoianVsaWVubXQiLCJhIjoiY2s5aGlyNTllMHY3dzNlanczdnRldnFzaCJ9.XzX5C2Y_iiX7Ob33KunkfQ",
+                  additionalOptions: {
+                    'accessToken':
+                        'pk.eyJ1IjoianVsaWVubXQiLCJhIjoiY2s5aGlyNTllMHY3dzNlanczdnRldnFzaCJ9.XzX5C2Y_iiX7Ob33KunkfQ',
+                    'id': 'mapbox.mapbox-streets-v11'
+                  }),
+              MarkerLayerOptions(
+                markers: _markers,
+              ),
+              MarkerLayerOptions(
+                markers: [
+                  Marker(
+                    // width: 50.0,
+                    // height: 50.0,
+                    point: LatLng(_position.latitude!, _position.longitude!),
+                    builder: (ctx) => const FlutterLogo(),
                   ),
-                  mapController: _mapController,
-                  nonRotatedLayers: [
-                    TileLayerOptions(
-                        urlTemplate:
-                            "https://api.mapbox.com/styles/v1/julienmt/ckx6t9wl86f0u14nqm714f3th/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoianVsaWVubXQiLCJhIjoiY2s5aGlyNTllMHY3dzNlanczdnRldnFzaCJ9.XzX5C2Y_iiX7Ob33KunkfQ",
-                        additionalOptions: {
-                          'accessToken':
-                              'pk.eyJ1IjoianVsaWVubXQiLCJhIjoiY2s5aGlyNTllMHY3dzNlanczdnRldnFzaCJ9.XzX5C2Y_iiX7Ob33KunkfQ',
-                          'id': 'mapbox.mapbox-streets-v11'
-                        }),
-                    MarkerLayerOptions(
-                      markers: _markers,
-                    ),
-                    MarkerLayerOptions(
-                      markers: [
-                        Marker(
-                          // width: 50.0,
-                          // height: 50.0,
-                          point:
-                              LatLng(_position.latitude!, _position.longitude!),
-                          builder: (ctx) => const FlutterLogo(),
-                        ),
-                      ],
-                    ),
-                  ],
-                )
-              : Column(
-                  children: const [
-                    Expanded(
-                        child: Center(
-                            child: CircularProgressIndicator(
-                                color: brandPrimaryColor))),
-                  ],
-                ),
+                ],
+              ),
+            ],
+          ),
           SafeArea(
             child: Padding(
               padding: const EdgeInsets.all(20),
@@ -124,8 +143,8 @@ class _NearbyStoreScreenState extends State<NearbyStoreScreen> {
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
                   FloatingActionButton(
-                    onPressed: () =>
-                        _gotoLocation(_position!.latitude!, _position.longitude!),
+                    onPressed: () => _gotoLocation(
+                        _position.latitude!, _position.longitude!),
                     child: const Icon(Icons.my_location),
                     backgroundColor: brandPrimaryColor,
                   ),
